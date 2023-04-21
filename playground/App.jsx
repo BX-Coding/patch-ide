d5078a8d20b4b9189d7e89784ca15dff0d4cff9d
Roe, Elliot Benjamin <eroe7@gatech.edu>
2023-04-21 11:38:13 -0400

    Took out all pyatch vm code so demo would run

--- playground/App.jsx
+++ playground/App.jsx
@@ -1,23 +1,25 @@
 import { useState } from 'react'
-import PyatchProvider from '../src/components/provider/PyatchProvider.jsx';
-import { PyatchStartButton } from '../src/components/PyatchButton.jsx';
-import PyatchStage from '../src/components/PyatchStage.jsx';
 import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button'
+import CodeMirror from '@uiw/react-codemirror';
+import { python } from '@codemirror/lang-python';
 import './App.css'
 
 function App() {
 
   return (
-    <PyatchProvider>
-      <Grid container spacing={2}>
-        <Grid item xs={12}>
-          <PyatchStage/>
-        </Grid>
-        <Grid item xs={8}>
-          <PyatchStartButton/>
-        </Grid>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <CodeMirror
+          value="console.log('hello world!');"
+          extensions={[python()]}
+          onChange={onChange}
+        />
       </Grid>
-    </PyatchProvider>
+      <Grid item xs={8}>
+        <Button variant="contained">Run</Button>
+      </Grid>
+    </Grid>
   )
 }
 
