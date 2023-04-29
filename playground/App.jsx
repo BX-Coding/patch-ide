41318e545dd4214fcfdeadb9d7d5a1e4087f595f
Roe, Elliot Benjamin <eroe7@gatech.edu>
2023-04-29 17:29:29 -0400

    Created basic front-end mock up

--- playground/App.jsx
+++ playground/App.jsx
@@ -2,22 +2,49 @@ import { useState } from 'react'
 import Grid from '@mui/material/Grid';
 import Button from '@mui/material/Button'
 import CodeMirror from '@uiw/react-codemirror';
+import { material } from '@uiw/codemirror-theme-material';
 import { python } from '@codemirror/lang-python';
+import FlagIcon from '@mui/icons-material/Flag';
+import DangerousIcon from '@mui/icons-material/Dangerous';
+import Typography from '@mui/material/Typography';
+import catImage from './scratchCat.png';
 import './App.css'
 
 function App() {
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
+    <Grid container width={'100%'} spacing={2}>
+      <Grid item xs={8}>
         <CodeMirror
-          value="console.log('hello world!');"
+          value="cat.move(10)"
           extensions={[python()]}
-          onChange={onChange}
+          theme={material}
+          height="90vh"
         />
       </Grid>
-      <Grid item xs={8}>
-        <Button variant="contained">Run</Button>
+      <Grid container item direction="column" xs={4} spacing={2}>
+        <Grid container item spacing={2}>
+          <Grid item>
+            <Button variant="contained"><FlagIcon/></Button>
+          </Grid>
+          <Grid item>
+            <Button variant="outlined"><DangerousIcon/></Button>
+          </Grid>
+        </Grid>
+        <Grid container item xs={6}>
+          <Grid item container justifyContent='center' alignItems={'center'} backgroundColor={'#E6E6E6'} borderColor={''} borderRadius={2} xs={12}>
+            <Grid item>
+              <img width={100} src={catImage}/>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid container item xs={5}>
+          <Grid item container justifyContent='center' alignItems={'center'} backgroundColor={'#2e3235'} borderColor={''} borderRadius={2} xs={12}>
+            <Typography gutterBottom variant="h4" component="div">
+              SPRITE AREA
+            </Typography>
+          </Grid>
+        </Grid>
       </Grid>
     </Grid>
   )
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import NodePolyfillPlugin from 'node-polyfill-webpack-plugin';
+
+import * as url from 'url';
+
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
+
+
+export default {
+  entry: path.join(__dirname, "playground", "main.jsx"),
+  output: {
+    path: path.join(__dirname, "/playground/public/assets"),
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.?(js|jsx|mjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              '@babel/preset-env', 
+              ['@babel/preset-react', {"runtime": "automatic"}]
+          ]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.sprite3$/,
+        use: ['arraybuffer-loader'],
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "playground", "public", "index.html"),
+    }),
+    new NodePolyfillPlugin(),
+  ],
+}
