afcbb3f1c51a84e1d1e9cbe78306daa330365201
Benjamin Montgomery <benjamin8yrs@gmail.com>
2023-07-06 16:16:46 -0400

    fix: deleting a sprite now removes "say" bubbles

--- src/components/PatchDeleteSprite.jsx
+++ src/components/PatchDeleteSprite.jsx
@@ -3,13 +3,17 @@ import Button from '@mui/material/Button';
 import pyatchContext from './provider/PyatchContext.js';
 import Grid from '@mui/material/Grid';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
+import Runtime from 'pyatch-vm/src/engine/runtime.mjs';
 
 export function PyatchDeleteSprite(props) {
-    const { editingTargetId, onDeleteSprite } = useContext(pyatchContext);
+    const { editingTargetId, pyatchVM, onDeleteSprite } = useContext(pyatchContext);
 
-    return(
+    return (
         <Grid container justifyContent="center">
-            <Button variant="contained" onClick={()=>onDeleteSprite(editingTargetId)} sx={{m:"1vh"}}><DeleteOutlineIcon/></Button>
+            <Button variant="contained" onClick={async () => {
+                pyatchVM.runtime.emit("targetWasRemoved", pyatchVM.editingTarget);
+                onDeleteSprite(editingTargetId);
+            }} sx={{ m: "1vh" }}><DeleteOutlineIcon /></Button>
         </Grid>
     );
 }
