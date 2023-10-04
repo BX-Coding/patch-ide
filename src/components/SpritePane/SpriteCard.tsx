bcfec9c792344c9eaef6e74f68aea0b151b5e9b0
Jared Lim <jaredorlim@gmail.com>
2023-10-04 09:59:55 -0400

    Refactored ternary operator

--- src/components/SpritePane/SpriteCard.tsx
+++ src/components/SpritePane/SpriteCard.tsx
@@ -13,48 +13,29 @@ type SpriteCardProps = {
 export function SpriteCard({ target }: SpriteCardProps) {
     const [editingTarget, setEditingTarget] = useEditingTarget();
 
-    let inputName = ''
-
-    // Upon a name change, re-render the sprite's card in a local variable.
-    // useEffect(() => {
-    //     if ((target?.sprite?.name).length > 16) {
-    //         inputName = (target?.sprite?.name).substring(0, 13) + '...'
-    //     }
-    //     else {
-    //         inputName = target?.sprite?.name
-    //     }
-
-    // }, [target?.sprite?.name])
-
     const onClick = () => {
         setEditingTarget(target.id);
     }
 
+    let titleName = target?.sprite?.name
+
+    if (target?.sprite?.name.length > 13) {
+        titleName = titleName.substring(0, 10) + '...'
+    }
+
     return (
         <>
-            {target?.sprite?.name.length > 13 ?
-                <ItemCard
-                    title={target?.sprite?.name.substring(0, 10) + '...'}
-                    selected={editingTarget?.id === target.id}
-                    onClick={onClick}
-                    key={target?.sprite?.name}
-                    width={120}
-                    height={120}
-                >
-                    <CostumeImage costume={target?.sprite?.costumes[0]} className="costumeCardImage" />
-                </ItemCard>
-                :
-                <ItemCard
-                    title={target?.sprite?.name}
-                    selected={editingTarget?.id === target.id}
-                    onClick={onClick}
-                    key={target?.sprite?.name}
-                    width={120}
-                    height={120}
-                >
-                    <CostumeImage costume={target?.sprite?.costumes[0]} className="costumeCardImage" />
-                </ItemCard>
-            }
+            <ItemCard
+                title={titleName}
+                selected={editingTarget?.id === target.id}
+                onClick={onClick}
+                key={target?.sprite?.name}
+                width={120}
+                height={120}
+            >
+                <CostumeImage costume={target?.sprite?.costumes[0]} className="costumeCardImage" />
+            </ItemCard>
+
         </>
 
 
