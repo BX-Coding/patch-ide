13d2556dfeeca0c9d825fc94060db70019065098
Roe, Elliot Benjamin <eroe7@gatech.edu>
2024-02-17 20:42:36 -0500

    fix: sprite images shown in modal

--- src/components/ItemCard/component.tsx
+++ src/components/ItemCard/component.tsx
@@ -1,38 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import { Box, Typography, Grid, IconProps } from '@mui/material';
-import { boxStyle, typographyStyle } from './style';
+import React, { useEffect, useState } from "react";
+import { Box, Typography, Grid, IconProps } from "@mui/material";
+import { boxStyle, typographyStyle } from "./style";
 
 type ItemCardProps = {
-    title: string,
-    selected: boolean,
-    onClick: (title: string) => void,
-    actionButtons?: JSX.Element[],
-    width: number,
-    height: number,
-    children?: React.JSX.Element,
-}
+  title: string;
+  selected: boolean;
+  onClick: (title: string) => void;
+  actionButtons?: JSX.Element[];
+  width: number;
+  height: number;
+  children?: React.JSX.Element;
+};
 
-export function ItemCard({ title, selected, onClick, actionButtons, width, height, children }: ItemCardProps) {
-    return (
-    <Box onClick={() => { onClick(title) }} sx={boxStyle(selected, height, width)}>
-        <Grid container justifyContent="space-between" direction="column" sx={{height}}>
-            <Grid item xs={8}>
-                <div style={{
-                    width: "100%",
-                    height: "100%",
-                }}>
-                {children}
-                </div>
-            </Grid>
-            <Grid container item xs={1} direction="row">
-                <Typography sx={typographyStyle}>{title}</Typography>
-                {actionButtons ? (actionButtons.map((button, i) => {
-                    return (
-                        <Grid item>{button}</Grid>
-                    );
-                })) : null}
-            </Grid>
+export function ItemCard({
+  title,
+  selected,
+  onClick,
+  actionButtons,
+  width,
+  height,
+  children,
+}: ItemCardProps) {
+  return (
+    <Box
+      onClick={() => {
+        onClick(title);
+      }}
+      sx={boxStyle(selected, height, width)}
+    >
+      <Grid
+        container
+        justifyContent="space-between"
+        direction="column"
+        sx={{ height, overflow: "hidden" }}
+      >
+        <Grid
+          item
+          xs={8}
+          sx={{
+            width: "100%",
+            height: "100%",
+          }}
+        >
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          >
+            {children}
+          </div>
         </Grid>
+        <Grid container item xs={1} direction="row">
+          <Typography sx={typographyStyle}>{title}</Typography>
+          {actionButtons
+            ? actionButtons.map((button, i) => {
+                return <Grid item>{button}</Grid>;
+              })
+            : null}
+        </Grid>
+      </Grid>
     </Box>
-    );
+  );
 }
