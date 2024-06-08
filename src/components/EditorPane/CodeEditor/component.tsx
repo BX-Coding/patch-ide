4098cb0bb205d23e80b503dc741648b591cafbd7
Leighton Miguel <leim56@hawaii.rr.com>
2024-06-07 16:19:25 -1000

    Unique Root URI Function

--- src/components/EditorPane/CodeEditor/component.tsx
+++ src/components/EditorPane/CodeEditor/component.tsx
@@ -16,13 +16,23 @@ export const CodeEditor = () => {
 
         // const serverUri = `ws://localhost:8081` as `ws://${string}` | `wss://${string}`;
         const serverUri = `${process.env.LSP_SERVER_URL}` as `ws://${string}` | `wss://${string}`;
+
+        function generateUniqueRootUri(length: number): string {
+            const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            let result = '';
+            const charactersLength = characters.length;
+            for (let i = 0; i < length; i++) {
+                result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            }
+            return result;
+        }
     
         const socket = new WebSocket(serverUri)
         setWebSocketRef(socket)
         
         const ls = languageServer({
             serverUri,
-            rootUri: "file:///",
+            rootUri: `file:///${generateUniqueRootUri(10)}`,
             documentUri: "file:///index.js",
             languageId: "python",
             workspaceFolders: null,
