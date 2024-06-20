52f2d3707fd22da4467603b32de8f043d520aec8
Leighton Miguel <leim56@hawaii.rr.com>
2024-06-19 14:10:46 -1000

    Update component.tsx

--- src/components/EditorPane/CodeEditor/component.tsx
+++ src/components/EditorPane/CodeEditor/component.tsx
@@ -21,12 +21,12 @@ export const CodeEditor = () => {
   const setTransport = usePatchStore((state)=>state.setTransportRef)
 
   useEffect(() => {
-    const serverUri = `ws://localhost:8000` as
-      | `ws://${string}`
-      | `wss://${string}`;
-    // const serverUri = `${process.env.LSP_SERVER_URL}` as
+    // const serverUri = `ws://localhost:8081` as
     //   | `ws://${string}`
     //   | `wss://${string}`;
+    const serverUri = `${process.env.LSP_SERVER_URL}` as
+      | `ws://${string}`
+      | `wss://${string}`;
 
     const transport = new WebSocketTransport(serverUri);
     setTransport(transport)
