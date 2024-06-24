0af585a0233c0de2910be5da702a5907a3c5b25f
Ava Wandersleben <78870469+Snoopy219@users.noreply.github.com>
2024-06-24 12:40:58 -0400

    show no image if none available

--- src/components/EditorPane/CodeEditor/HoverTooltip/component.tsx
+++ src/components/EditorPane/CodeEditor/HoverTooltip/component.tsx
@@ -14,7 +14,7 @@ export const HoverTooltip = (props:HoverTooltipProps) => {
         <Typography variant="body1">{props.declare}</Typography>
         <Typography variant="body2">{props.descript} </Typography>
         <Typography variant="body2">Exmaple: {props.exampleCode}</Typography>
-        <img src={props.imgSrc} onError={({currentTarget})=> {currentTarget.src = "https://firebasestorage.googleapis.com/v0/b/patch-271d1.appspot.com/o/gifs%2Fshow.gif?alt=media"}} width={300}/>
+        <img src={props.imgSrc} alt="" width={300}/>
     </div>
   );
 };
