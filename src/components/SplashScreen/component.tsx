33153e1a5f374e20d62214d7e10ca78a8da7466c
Roe, Elliot Benjamin <eroe7@gatech.edu>
2024-06-24 19:48:07 -0400

    style: fixed the weird drop shadow original implementation

--- src/components/SplashScreen/component.tsx
+++ src/components/SplashScreen/component.tsx
@@ -1,36 +1,55 @@
-import { Box, Typography } from '@mui/material';
-import React, { useEffect, useState } from 'react';
+import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
 // @ts-ignore
-import patchPenguin from '../../assets/PatchPenguin.svg';
+import patchPenguin from "../../assets/PatchPenguin.png";
 
 type SplashScreenProps = {
-    renderCondition: boolean,
-    children?: React.ReactNode,
-}
+  renderCondition: boolean;
+  children?: React.ReactNode;
+};
 const SplashScreen = ({ renderCondition, children }: SplashScreenProps) => {
-    return <>
-        {!renderCondition && <PatchAnimation/>}
-        <div style={{display: renderCondition ? "block" : "none"}}>{children}</div>
+  return (
+    <>
+      {!renderCondition && <PatchAnimation />}
+      <div style={{ display: renderCondition ? "block" : "none" }}>
+        {children}
+      </div>
     </>
-}
+  );
+};
 
 const PatchAnimation = () => {
-    const [loadingText, setLoadingText] = useState("Loading")
-    const [loadingDots, setLoadingDots] = useState("")
+  const [loadingText, setLoadingText] = useState("Loading");
+  const [loadingDots, setLoadingDots] = useState("");
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setLoadingDots(loadingDots => loadingDots.length < 3 ? loadingDots + "." : "");
-        }, 500);
-        return () => clearInterval(interval);
-    }, []);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setLoadingDots((loadingDots) =>
+        loadingDots.length < 3 ? loadingDots + "." : ""
+      );
+    }, 500);
+    return () => clearInterval(interval);
+  }, []);
 
-    return(
-        <Box sx={{width: "100vw", height: "100vh", display: "flex", margin: "-8px", justifyContent: "center", alignItems: "center", backgroundColor: "background.default", flexDirection: "column"}}>
-            <img style={{boxShadow: "rgba(0, 0, 0, 0.35) 0px 5px 15px", height: "50%"}} src={patchPenguin}/>
-            <Typography sx={{fontSize: "24pt", padding: "8px"}}>{`${loadingText}${loadingDots}`}</Typography>
-        </Box>
-    );
-}
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        margin: "-8px",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "background.default",
+        flexDirection: "column",
+      }}
+    >
+      <img style={{ height: "50%" }} src={patchPenguin} />
+      <Typography
+        sx={{ fontSize: "24pt", padding: "8px", color: "white" }}
+      >{`${loadingText}${loadingDots}`}</Typography>
+    </Box>
+  );
+};
 
 export default SplashScreen;
