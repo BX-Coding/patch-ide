6fc921302d555ac289289ecab031ab607670aa90
Roe, Elliot Benjamin <eroe7@gatech.edu>
2024-06-24 19:30:49 -0400

    added a fixed image height in-order to prevent the image from loading late and then covering the cursor

--- src/components/EditorPane/CodeEditor/HoverTooltip/component.tsx
+++ src/components/EditorPane/CodeEditor/HoverTooltip/component.tsx
@@ -2,19 +2,29 @@ import { Typography } from "@mui/material";
 import React from "react";
 
 type HoverTooltipProps = {
-    declare: string,
-    descript: string,
-    exampleCode: string,
-    imgSrc: string
+  declare: string;
+  descript: string;
+  exampleCode: string;
+  imgSrc: string;
 };
-export const HoverTooltip = (props:HoverTooltipProps) => {
-
+export const HoverTooltip = (props: HoverTooltipProps) => {
   return (
-    <div style={{ width: 300 }}>
-        <Typography variant="body1">{props.declare}</Typography>
-        <Typography variant="body2">{props.descript} </Typography>
-        <Typography variant="body2">Exmaple: {props.exampleCode}</Typography>
-        <img src={props.imgSrc} alt="" width={300}/>
+    <div
+      style={{
+        width: 300,
+        border: "1px solid rgba(255, 255, 255, 0.1)",
+        borderRadius: "5px",
+        padding: "5px",
+      }}
+    >
+      <Typography variant="body1">{props.declare}</Typography>
+      <Typography variant="body2">{props.descript} </Typography>
+      <Typography variant="body2">Example: {props.exampleCode}</Typography>
+      {props.imgSrc !== "" ? (
+        <img src={props.imgSrc} alt="" width={300} height={200} />
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
