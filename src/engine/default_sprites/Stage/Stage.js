3a64b7a6ee49814b509c1664894988007376380b
Leighton Miguel <leim56@hawaii.rr.com>
2024-07-18 13:02:06 -1000

    Move default project assets

--- src/engine/default_sprites/Stage/Stage.js
+++ src/engine/default_sprites/Stage/Stage.js
@@ -11,17 +11,17 @@ export default class Stage extends StageBase {
     super(...args);
 
     this.costumes = [
-      new Costume("backdrop1", "./Stage/costumes/backdrop1.svg", {
+      new Costume("backdrop1", "/backdrop1.svg", {
         x: 125.00153898879995,
         y: 156.4825870646767
       }),
-      new Costume("scratchCat", "./Stage/costumes/scratchCat.png", {
+      new Costume("scratchCat", "/scratchCat.png", {
         x: 402,
         y: 160
       }),
-      new Costume("gobo", "./Stage/costumes/gobo.png", { x: 294, y: 86.5 }),
-      new Costume("giga", "./Stage/costumes/giga.png", { x: 344, y: 145 }),
-      new Costume("blank", "./Stage/costumes/blank.svg")
+      new Costume("gobo", "/gobo.png", { x: 294, y: 86.5 }),
+      new Costume("giga", "/giga.png", { x: 344, y: 145 }),
+      new Costume("blank", "/blank.svg")
     ];
 
     this.triggers = [new Trigger(Trigger.CLICKED, this.whenstageclicked)];
