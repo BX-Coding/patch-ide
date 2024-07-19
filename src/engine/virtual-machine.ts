de55b0f72d4ccb8f2cc6ad8401ad77d551dbeb7d
plenjamin <53486418+benmontycomputer@users.noreply.github.com>
2024-07-19 16:11:48 -0400

    small change to comments in virtual-machine.ts

--- src/engine/virtual-machine.ts
+++ src/engine/virtual-machine.ts
@@ -272,7 +272,7 @@ export default class VirtualMachine extends EventEmitter {
     }
 
     addSprite(sprite: Sprite | SpriteJson, name: string) {
-
+        // TODO: implement this
     }
 
     /**
@@ -558,7 +558,7 @@ export default class VirtualMachine extends EventEmitter {
 
         return characterKeys.concat(scratchKeys);*/
 
-        // TODO: implement this
+        // TODO: implement this? idk if its needed
     }
 
     // There is 100% a better way to implement this
